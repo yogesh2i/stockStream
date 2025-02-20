e5476b2f405993c114f593374479247942b5e39b
yogesh <yogirao8440@gmail.com>
2025-02-20 19:32:53 +0530

    update: port util

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 const grpc = require("@grpc/grpc-js");
 const protoloader = require("@grpc/proto-loader");
+const { port, ticker } = require("./utils");
 const PROTO_PATH = __dirname+"/protos/stockLive.proto";
 
 const packageDefinition = protoloader.loadSync(PROTO_PATH);
 const stockLive_proto =  grpc.loadPackageDefinition(packageDefinition).stockLive;
-const client = new stockLive_proto("0.0.0.0:50050",grpc.credentials.createInsecure());
+const client = new stockLive_proto(`0.0.0.0:${port}`,grpc.credentials.createInsecure());
 
 function getData(){
         const call = client.stockStream();
-        call.write({"ticker":"AAPL"});
+        call.write({"ticker":ticker});
         call.on("data",(res)=>{
           console.log(res);
         })
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const grpc = require("@grpc/grpc-js");
 const {server}  = require("./grpcServer");
+const { port } = require("./utils");
 
 
-server.bindAsync("0.0.0.0:50050",grpc.ServerCredentials.createInsecure(),(err,port)=>{
+server.bindAsync(`0.0.0.0:${port}`,grpc.ServerCredentials.createInsecure(),(err,port)=>{
     if(err){
      console.log("Failed to create server!"+err.message);
     }else{
